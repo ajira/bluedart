c575f3fb656f3e701869f2cca38217ca5039fa95
ShimoJS <shimo@ajirasoft.com>
2020-07-11 20:45:04 +0530

    Use right namespaces for cancel pickup request

--- lib/bluedart/cancel_waybill.rb
+++ lib/bluedart/cancel_waybill.rb
@@ -22,6 +22,36 @@ module Bluedart
         opts = {message: 'CancelWaybill', wsa: wsa, params: params, extra: {'Profile' => @profile}, url: request_url}
         make_request(opts)
       end
-  
+
+      private
+
+      def namespaces
+        ns = {}
+        ns[:envelope] = {key:'env', value: 'http://www.w3.org/2003/05/soap-envelope'}
+        ns[:content]  = {key:'ns2', value: 'http://tempuri.org/'}
+        ns[:profile]  = {key:'ns3', value: 'http://schemas.datacontract.org/2004/07/SAPI.Entities.Admin'}
+        ns[:request]    = {key:'ns1', value: 'http://schemas.datacontract.org/2004/07/SAPI.Entities.WayBillGeneration'}
+        ns[:wsa]      = {key:'ns4', value: 'http://www.w3.org/2005/08/addressing'}
+        ns
+      end
+
+      def body_xml(xml, message, params, extra)
+        content_ns_key = "#{namespace_key(:content)}"
+        xml.Body {
+          xml[content_ns_key].send(message) do |xml|
+            xml[content_ns_key].send("Request") { awb_xml xml }
+            extra.each do |key, value|
+              xml[content_ns_key].send(key) { profile_xml(xml, value)} if key.downcase == 'profile'
+            end
+          end
+        }
+        xml
+      end
+
+      def awb_xml(xml)
+        xml["ns1"].AWBNo @AWBNo
+        xml 
+      end
+
     end
   end
